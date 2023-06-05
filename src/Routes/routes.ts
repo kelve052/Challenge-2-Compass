9e4b0fec38fb2cf2c8d5b1a3c0acff4420212c3b
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2023-06-05 12:04:29 -0300

    organizando comentarios em rotas

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
 const router = express.Router()
 import tutors from "../controllers/tutors";
-//get and post
-router.route('/tutors').get(tutors.TutorsGet).post(tutors.TutorsPost)
 
-//delete and put
+//rotas turor
+router.route('/tutors').get(tutors.TutorsGet).post(tutors.TutorsPost)
 router.route('/tutor/:id').delete(tutors.DeleteTutor).put(tutors.PutTutor)
 
+//rotas pet
 router.route('/pet/:tutorId').post(tutors.PetPost)
 router.route('/pet/:petId/tutor/:tutorId').put(tutors.PutPet).delete(tutors.DeletePet)
 
